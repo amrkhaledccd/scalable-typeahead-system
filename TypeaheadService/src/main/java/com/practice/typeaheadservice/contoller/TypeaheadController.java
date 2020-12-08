2a8234cdbd409b0b80abc44fa3913bc19914d5b7
swapyinc <swapyinc@gmail.com>
2020-12-08 18:51:09 +0100

    remove unused imports

--- TypeaheadService/src/main/java/com/practice/typeaheadservice/contoller/TypeaheadController.java
+++ TypeaheadService/src/main/java/com/practice/typeaheadservice/contoller/TypeaheadController.java
@@ -1,13 +1,11 @@
 package com.practice.typeaheadservice.contoller;
 
 import com.practice.typeaheadservice.domain.Phrase;
-import com.practice.typeaheadservice.domain.TrieNode;
 import com.practice.typeaheadservice.service.TrieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
