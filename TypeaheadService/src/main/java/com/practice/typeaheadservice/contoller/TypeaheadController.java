88eec786cec603086106a49028aebef40ab6e58f
swapyinc <swapyinc@gmail.com>
2020-12-13 16:39:16 +0100

    change addPhrase request payload

--- TypeaheadService/src/main/java/com/practice/typeaheadservice/contoller/TypeaheadController.java
+++ TypeaheadService/src/main/java/com/practice/typeaheadservice/contoller/TypeaheadController.java
@@ -1,6 +1,7 @@
 package com.practice.typeaheadservice.contoller;
 
 import com.practice.typeaheadservice.domain.Phrase;
+import com.practice.typeaheadservice.payload.AddPhraseRequest;
 import com.practice.typeaheadservice.service.TrieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,9 @@ public class TypeaheadController {
     @Autowired private TrieService trieService;
 
     @PostMapping("/trie/phrase")
-    public ResponseEntity<?> addPhrase(@RequestBody String phrase) {
-        log.info("Adding phrase {} to the trie", phrase);
-        trieService.addPhrase(phrase);
+    public ResponseEntity<?> addPhrase(@RequestBody AddPhraseRequest request) {
+        log.info("Adding phrase {} to the trie", request.getText());
+        trieService.addPhrase(request.getText());
         return ResponseEntity.accepted().build();
     }
 
--- TypeaheadService/src/main/java/com/practice/typeaheadservice/payload/AddPhraseRequest.java
+++ TypeaheadService/src/main/java/com/practice/typeaheadservice/payload/AddPhraseRequest.java
@@ -1,2 +1,14 @@
-package com.practice.typeaheadservice.payload;public class AddPhraseRequest {
+package com.practice.typeaheadservice.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddPhraseRequest {
+    private String text;
 }
